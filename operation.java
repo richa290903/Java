5d9a2f695f79d4254ff38042a723918573423736
richa290903 <155316586+richa290903@users.noreply.github.com>
2024-01-14 12:07:20 +0530

    simple

---
+++ operation.java
@@ -0,0 +1,9 @@
+class operation {
+      public static void main(String[] args) {
+            byte a=20,b=10;
+		System.out.println("Addition is :"+(a+b));
+		System.out.println("Subtraction is :"+(a-b));
+		System.out.println("Multiplication is :"+(a*b));
+		System.out.println("Division is :"+(a/b));
+      }      
+}

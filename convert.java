c9ca5304e204fec37fc27575a2f67ad1c125cea4
richa290903 <155316586+richa290903@users.noreply.github.com>
2024-01-14 12:06:47 +0530

    convert

---
+++ convert.java
@@ -0,0 +1,10 @@
+class convert {
+      public static void main(String[] args) {
+            int day=730,year,month;
+		month=day/30;
+		year=month/12;
+            System.out.println("Days are convert into month and year :"+day);
+		System.out.println("Month :"+month);
+		System.out.println("Year :"+year);
+      }      
+}

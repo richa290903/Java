7c2cdef124ba24c2062a785fcbc9736352c59b03
richa290903 <155316586+richa290903@users.noreply.github.com>
2024-01-14 12:11:05 +0530

    pattern

---
+++ pattern.java
@@ -0,0 +1,20 @@
+/*      *
+       **
+      ***
+     ****
+    *****        */
+
+class pattern {
+      public static void main(String[] args) {
+            int i,j,n=5;
+            for (i = n; i >= 1; i--){
+                  for (j = 1; j < i; j++) {
+                      System.out.print(" ");
+                  }
+                  for (j = 0; j <= n - i; j++) {
+                      System.out.print("*");
+                  }
+                  System.out.println();
+            }  
+      }    
+}

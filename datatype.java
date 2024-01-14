d9a822d59f945b50c89659d6195a7b6669c31305
richa290903 <155316586+richa290903@users.noreply.github.com>
2024-01-14 12:04:59 +0530

    data type

---
+++ datatype.java
@@ -0,0 +1,22 @@
+class datatype {
+      public static void main(String args[]){
+            byte b1=10;
+            short s1=100;
+            int i1=1000;
+            long l1=10000;
+            float f1=5.77f;
+            double d1=0.005;
+            char c1='A';
+            boolean b2=true;
+            String str="Abc def";
+            System.out.println("Byte :"+b1);
+            System.out.println("Short :"+s1);
+            System.out.println("Integer :"+i1);
+            System.out.println("Long :"+l1);
+            System.out.println("Float :"+f1);
+            System.out.println("Double :"+d1);
+            System.out.println("Char :"+c1);
+            System.out.println("Boolean :"+b2);
+            System.out.println("String :"+str);
+      }
+}

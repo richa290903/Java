6322ee55b02f0124fbbd17c71099125e00811d24
richa290903 <155316586+richa290903@users.noreply.github.com>
2024-01-14 12:10:21 +0530

    Descending order

---
+++ descending.java
@@ -0,0 +1,28 @@
+class descending {
+      public static void main(String[] args) {
+            int x1[]={20,30,50,10,40};
+			int i,j,tmp;
+			for(i=0;i<5;i++)
+			{
+				System.out.print(x1[i] + "   ");
+			}
+			for(i=1;i<5;i++)
+			{
+				for(j=0;j<(x1.length)-1;j++)
+				{
+					if(x1[j]<x1[j+1])
+					{
+						tmp=x1[j];
+						x1[j]=x1[j+1];
+						x1[j+1]=tmp;
+					}
+				}
+			}
+			System.out.println();
+			System.out.println("Array in descending order..........");
+			for(i=0;i<5;i++)
+			{
+				System.out.print(x1[i] + "   ");
+			}
+      }      
+}

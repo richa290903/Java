1419768fcff166ec90578d2ce6bd76873f66dab9
richa290903 <155316586+richa290903@users.noreply.github.com>
2024-01-27 23:29:22 +0530

    dynamic

--- matrix.java
+++ matrix.java
@@ -1,51 +1,80 @@
+/*Write a program to create two 3*3 matrix and perform addition, 
+subtraction, multiplication operation. */
+import java.util.Scanner;
+
 class matrix {
       public static void main(String[] args) {
-            int x1[][]={{1,1,1},{2,2,2},{3,3,3}};
-		int x2[][]={{4,4,4},{3,3,3},{5,5,5}};	
-		int x3[][]= new int[3][3];
-		int i,j,k;
+		int i,j,k,row,col;
+		Scanner s1 = new Scanner(System.in);
+            System.out.print("Enter row size :");	
+            row=s1.nextInt();
+		System.out.print("Enter column size :");
+            col=s1.nextInt();
+            int x1[][]=new int[row][col];	
+		int x2[][]=new int[row][col];
+		int x3[][]=new int[row][col];
 		System.out.println("First Matrix is..........");
-		for(i=0;i<3;i++)
+		for(i=0;i<row;i++)
 		{
-			for(j=0;j<3;j++)
+			for(j=0;j<col;j++)
 			{
-				System.out.print(x1[i][j] + "   ");
+				System.out.print("Enter value for x1["+i+"]["+j+"]:");
+				x1[i][j]=s1.nextInt();
 			}
 			System.out.println();
 		}
 		System.out.println("Second Matrix is..........");
-		for(i=0;i<3;i++)
+		for(i=0;i<row;i++)
 		{
-			for(j=0;j<3;j++)
+			for(j=0;j<col;j++)
 			{
-				System.out.print(x2[i][j] + "   ");
+				System.out.print("Enter value for x1["+i+"]["+j+"]:");
+				x2[i][j]=s1.nextInt();
+			}
+			System.out.println();
+		}
+		System.out.println("First Matrix is..........");
+		for(i=0;i<row;i++)
+		{
+			for(j=0;j<col;j++)
+			{
+				System.out.print(x1[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		System.out.println("Second Matrix is..........");
+		for(i=0;i<row;i++)
+		{
+			for(j=0;j<col;j++)
+			{
+				System.out.print(x2[i][j]+"\t");
 			}
 			System.out.println();
 		}
 		System.out.println("Addition of two Matrix is..........");
-		for(i=0;i<3;i++)
+		for(i=0;i<row;i++)
 		{
-			for(j=0;j<3;j++)
+			for(j=0;j<col;j++)
 			{
 				System.out.print(x1[i][j]+x2[i][j] + "   ");
 			}
 			System.out.println();
 		}
 		System.out.println("Subtraction of two Matrix is..........");
-		for(i=0;i<3;i++)
+		for(i=0;i<row;i++)
 		{
-			for(j=0;j<3;j++)
+			for(j=0;j<col;j++)
 			{
 				System.out.print(x1[i][j]-x2[i][j] + "   ");
 			}
 			System.out.println();
 		}
 		System.out.println("Multiplication of two Matrix is..........");
-		for(i=0;i<3;i++)
+		for(i=0;i<row;i++)
 		{
-			for(j=0;j<3;j++)
+			for(j=0;j<col;j++)
 			{	
-				for(k=0;k<3;k++)
+				for(k=0;k<x3.length;k++)
 				{
 					x3[i][j]=x3[i][j]+x1[i][k]*x2[k][j];
 				}
